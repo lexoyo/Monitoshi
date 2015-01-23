f45353db4cf3d42a559a03dc64667281534d7790
lexoyo <a.hoyau@silexlabs.org>
2015-01-23 12:55:36 +0100

    added post requests to web hooks

--- app/alert/web-hook.js
+++ app/alert/web-hook.js
@@ -2,6 +2,7 @@
 var http = require('http');
 var https = require('https');
 var util = require('util');
+var querystring = require('querystring');
 
 /**
  * alerts to a given URL
@@ -24,14 +25,35 @@ module.exports = WebHookAlert = function(config) {
  * send notification
  */
 WebHookAlert.prototype.send = function(status, message) {
-    var url = util.format(this.config.url + '?' + this.config.params, status, message);
-    console.log('alert sent', url);
     var protocol = http;
     if (this.config.url.indexOf('https') === 0) {
         protocol = https;
     }
-    var req = protocol.get(url, function(res) {
-        console.log('alert sent done', res.statusCode);
-    }.bind(this));
+    if (this.config.method === 'get') {
+        var url = util.format(this.config.url + '?' + this.config.params, status, message);
+        console.log('alert sent', url);
+        var req = protocol.get(url, function(res) {
+            console.log('alert sent done', res.statusCode);
+        }.bind(this));
+    }
+    else {
+        var post_data = querystring.stringify({
+            status: status,
+            message: message,
+        });
+        var post_options = this.config.post_options;
+        post_options.headers['Content-Length'] = post_data.length;
+        console.log('alert sent', post_data, post_options);
 
+        // Set up the request
+        var post_req = protocol.request(post_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('alert post Response: ' + chunk);
+            });
+        });
+        // post the data
+        post_req.write(post_data);
+        post_req.end();
+    }
 };
