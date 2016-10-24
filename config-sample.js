06175f2894efe73fc73ceeeb685063b8c7241e1d
MB <billy321@im-paris.fr>
2016-10-24 12:54:02 +0200

    better config sample

--- config-sample.js
+++ config-sample.js
@@ -3,11 +3,11 @@ module.exports = {
     "timeout": 10000,
     "attempts": 3,
     "nodemailer": {
-        comment: 'gmail is complicated since https://nodemailer.com/using-gmail/ you will probably need smtp'
-        service: 'gmail',
-        auth: {
-            user: 'sender@gmail.com',
-            pass: 'password'
+        "comment": 'gmail is complicated since https://nodemailer.com/using-gmail/ you will probably need smtp',
+        "service": 'gmail',
+        "auth": {
+            "user": 'sender@gmail.com',
+            "pass": 'password'
         }
     }
 };
